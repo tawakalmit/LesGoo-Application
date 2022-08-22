29fd3a3ca3a5dd0a6a0f48793a0b77288ac17558
tawakalmit <tawakal.mit@gmail.com>
2022-08-22 16:57:42 +0700

    PWA has been added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   images: {
     domains: ['lesgooproject.s3.ap-southeast-1.amazonaws.com'],
   },
-};
+});
